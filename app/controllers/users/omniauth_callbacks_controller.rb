c3b554e725b7d42b838f8e29eb1f711f2f6bc40a
nightsoutstaff <nightsoutstaff@gmail.com>
2017-04-06 14:16:36 +0200

    sistemato accesso facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,18 +6,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     #end
     
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"], flash)
-    #if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+    @user = User.from_omniauth(request.env["omniauth.auth"])#, flash)
+    if @user.persisted?
+      sign_in @user#, :event => :authentication #this will throw if @user is not activated
+      redirect_to edit_user_registration_path
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    #else
-     # session["devise.facebook_data"] = request.env["omniauth.auth"]
-      #redirect_to new_user_registration_path
-    #end
+    else
+     session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_path
+    end
   end
 
   def failure
-    redirect_to root_path
+    redirect_to new_user_registration_path
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,25 +18,25 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
 
-  def self.from_omniauth(auth, f)
+  def self.from_omniauth(auth)#, f)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
       user.name = auth.info.name
       user.password = Devise.friendly_token[0,20]
-      f[:notice]="questa è la tua password per modificare il tuo account: "+"'"+user.password+"' "+"questo messaggio non verrà più mostrato quindi bisogna salvarsi questa password"
+      #f[:notice]="questa è la tua password per modificare il tuo account: "+"'"+user.password+"' "+"questo messaggio non verrà più mostrato quindi bisogna salvarsi questa password"
       #user.skip_confirmation!
     end
 	end
   
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-		end 
-	end
+  #def self.new_with_session(params, session)
+   # super.tap do |user|
+    #  if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+     #   user.email = data["email"] if user.email.blank?
+      #end
+		#end 
+	#end
 
   ROLES = %i[client owner]  # + admin e banned non a scelta dell'utente
 
